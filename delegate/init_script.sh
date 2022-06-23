4343d3c236d2fb1d9fbfd43f3beacacd438c4637
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2022-06-23 15:57:48 -0400

    Update init_script.sh

--- delegate/init_script.sh
+++ delegate/init_script.sh
@@ -1,4 +1,5 @@
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+#curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
 #apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+curl -fsSL https://apt.releases.hashicorp.com/gpg | apt-key add -
 apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com focal main"
 apt-get update && sudo apt-get install terraform
